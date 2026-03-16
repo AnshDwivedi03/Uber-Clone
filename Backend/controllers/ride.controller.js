1290829ee0592f1377a38af543083d9518e66ba1
Ansh Dwivedi <138333395+AnshDwivedi03@users.noreply.github.com>
2026-03-16 18:39:19 +0530

    Update ride.controller.js

--- Backend/controllers/ride.controller.js
+++ Backend/controllers/ride.controller.js
@@ -4,33 +4,42 @@ const mapService = require('../services/maps.service');
 const { sendMessageToSocketId } = require('../socket');
 const rideModel = require('../models/ride.model');
 
-
 module.exports.createRide = async (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    const { userId, pickup, destination, vehicleType, vibe, bid } = req.body;
+    // ADDED: Accept explicit coordinates from the frontend
+    const { userId, pickup, destination, pickupLat, pickupLng, destLat, destLng, vehicleType, vibe, bid } = req.body;
     console.log('createRide params:', { userId, pickup, destination, vehicleType, vibe, bid });
     require('fs').writeFileSync('debug_ride.log', JSON.stringify(req.body, null, 2) + '\n', { flag: 'a' });
 
     try {
-        const ride = await rideService.createRide({ user: req.user._id, pickup, destination, vehicleType, vibe, bid });
+        // Pass coordinates down to the service to bypass geocoding
+        const ride = await rideService.createRide({ 
+            user: req.user._id, 
+            pickup, 
+            destination, 
+            pickupLat, 
+            pickupLng, 
+            destLat, 
+            destLng,
+            vehicleType, 
+            vibe, 
+            bid 
+        });
+        
         console.log('Ride created with Vibe:', ride.vibe);
         res.status(201).json(ride);
 
-        const rideWithUser = await rideModel.findOne({ _id: ride._id }).populate('rider');
-
-        // Notification is now handled by the distributed Captain Service via RabbitMQ
-
+        // Notification is handled by the distributed Captain Service via RabbitMQ
     } catch (err) {
         if (err.message === 'Unable to fetch coordinates' || err.message === 'Unable to fetch distance and time') {
              return res.status(400).json({ message: err.message });
         }
         next(err);
     }
-
 };
 
 module.exports.getFare = async (req, res, next) => {
@@ -65,22 +74,7 @@ module.exports.confirmRide = async (req, res, next) => {
         const captainService = require('../services/captain.service');
         const result = await captainService.confirmRide({ rideId, captainId: req.captain._id });
 
-        // Since it's async now, we return a processing status or wait?
-        // For better UX, we might want to wait for the DB update or just return OK.
-        // Returning OK and letting Socket handle the UI update is standard for async.
         return res.status(200).json(result);
-
-        /* 
-        // Original Logic moved to async consumer
-        const ride = await rideService.confirmRide({ rideId, captain: req.captain });
-
-        sendMessageToSocketId(ride.user.socketId, {
-            event: 'ride-confirmed',
-            data: ride
-        })
-
-        return res.status(200).json(ride);
-        */
     } catch (err) {
         next(err);
     }
@@ -96,7 +90,6 @@ module.exports.startRide = async (req, res, next) => {
 
     try {
         const ride = await rideService.startRide({ rideId, otp, captain: req.captain });
-
         console.log(ride);
 
         sendMessageToSocketId(ride.rider.socketId, {
@@ -117,7 +110,6 @@ module.exports.endRide = async (req, res, next) => {
     }
 
     const { rideId } = req.body;
-
     console.log('End Ride Request received for:', { rideId, captainId: req.captain?._id });
 
     try {
@@ -127,8 +119,6 @@ module.exports.endRide = async (req, res, next) => {
             event: 'ride-ended',
             data: ride
         })
-
-
 
         return res.status(200).json(ride);
     } catch (err) {
